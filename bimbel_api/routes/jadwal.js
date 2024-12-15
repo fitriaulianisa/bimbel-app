cfbb9db036007971a9d682727e6ac87e1bd8ca84
fitriaulianisa <aulianisafitri434@gmail.com>
2024-12-15 13:48:58 +0700

    comment auth dan role

--- bimbel_api/routes/jadwal.js
+++ bimbel_api/routes/jadwal.js
@@ -7,10 +7,16 @@ const jadwalController = require("../controllers/jadwalController");
 // impor middleware auth dan role
 // const authMiddleware = require("../middleware/authMiddleware");
 // const roleMiddleware = require("../middleware/roleMiddleware");
-router.get("/", authMiddleware, roleMiddleware("admin"), jadwalController.getAllJadwal);
-router.post("/", authMiddleware, roleMiddleware('admin'), jadwalController.createJadwal);
-router.get("/:id", authMiddleware, jadwalController.getJadwalById);
-router.put("/:id", authMiddleware, roleMiddleware("admin"), jadwalController.updateJadwal);
-router.delete("/:id", authMiddleware, roleMiddleware("admin"),jadwalController.deleteJadwal);
+// router.get("/", authMiddleware, roleMiddleware("admin"), jadwalController.getAllJadwal);
+// router.post("/", authMiddleware, roleMiddleware('admin'), jadwalController.createJadwal);
+// router.get("/:id", authMiddleware, jadwalController.getJadwalById);
+// router.put("/:id", authMiddleware, roleMiddleware("admin"), jadwalController.updateJadwal);
+// router.delete("/:id", authMiddleware, roleMiddleware("admin"),jadwalController.deleteJadwal);
+
+router.get("/", jadwalController.getAllJadwal);
+router.post("/", jadwalController.createJadwal);
+router.get("/:id", jadwalController.getJadwalById);
+router.put("/:id", jadwalController.updateJadwal);
+router.delete("/:id", jadwalController.deleteJadwal);
 
 module.exports = router;
