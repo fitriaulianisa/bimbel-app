a3d1a555b7058c62ac794fe5666a40e0ce4feb3d
fitriaulianisa <aulianisafitri434@gmail.com>
2024-12-27 22:31:02 +0700

    update

--- bimbel_api/middleware/uploadMiddleware.js
+++ bimbel_api/middleware/uploadMiddleware.js
@@ -33,7 +33,7 @@ const fileFilter = (req, file, cb) => {
 
 const upload = multer({
     storage,
-    limits: { fileSize: 10 * 1024 * 1024 },
+    limits: { fileSize: 100 * 1024 * 1024 },
     fileFilter,
 
 });
