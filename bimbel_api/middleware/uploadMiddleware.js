faaf316e6a7859dbad6561fac51a00c03cd2e2f1
fitriaulianisa <aulianisafitri434@gmail.com>
2024-12-27 22:52:09 +0700

    update

--- bimbel_api/middleware/uploadMiddleware.js
+++ bimbel_api/middleware/uploadMiddleware.js
@@ -17,7 +17,7 @@ const storage = multer.diskStorage({
 
 });
 const fileFilter = (req, file, cb) => {
-    const allowedTypes = /jpeg|jpg|png|gif/;
+    const allowedTypes = /jpeg|jpg|png|gif|docx|doc|ppt|pptx/;
     const extname = allowedTypes.test(
         path.extname(file.originalname).toLowerCase()
     );
