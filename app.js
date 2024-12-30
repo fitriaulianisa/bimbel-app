fdca02efb8f127a658d6c0282d95c3f5c903559e
fitriaulianisa <aulianisafitri434@gmail.com>
2024-12-30 17:06:33 +0700

    update

--- app.js
+++ app.js
@@ -64,14 +64,20 @@ app.use('/api/auth', authRouterApi);
 app.use(function (req, res, next) {
   next(createError(404));
 });
-
-// Error handler
 app.use(function (err, req, res, next) {
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  res.status(err.status || 500);
-  res.render('error');
+  res.status(err.status || 500).json({
+    message: err.message,
+    error: req.app.get('env') === 'development' ? err : {}
+  });
 });
+
+// // Error handler
+// app.use(function (err, req, res, next) {
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
 
 module.exports = app;
