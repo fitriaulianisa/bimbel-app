15a28915f62efbf628bc700b7dd8c4d04c36f278
fitriaulianisa <aulianisafitri434@gmail.com>
2025-01-02 23:59:10 +0700

    update

--- bimbel_api/routes/guru.js
+++ bimbel_api/routes/guru.js
@@ -7,7 +7,7 @@ const guruController = require("../controllers/guruController");
 // impor middleware auth dan role
 const authMiddleware = require("../middleware/authMiddleware");
 const roleMiddleware = require("../middleware/roleMiddleware");
-router.get("/", guruController.getAllGuru);
+router.get("/", authMiddleware, roleMiddleware('admin'), guruController.getAllGuru);
 router.post("/", authMiddleware, roleMiddleware('admin'), guruController.createGuru);
 router.get("/:id", authMiddleware, guruController.getGuruById);
 router.put("/:id", authMiddleware, roleMiddleware("admin"), guruController.updateGuru);
