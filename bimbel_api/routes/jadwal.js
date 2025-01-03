66a3890b1982f51af9caf87482383317036f7f5c
fitriaulianisa <aulianisafitri434@gmail.com>
2025-01-03 20:27:53 +0700

    update

--- bimbel_api/routes/jadwal.js
+++ bimbel_api/routes/jadwal.js
@@ -7,9 +7,9 @@ const jadwalController = require("../controllers/jadwalController");
 // impor middleware auth dan role
 const authMiddleware = require("../middleware/authMiddleware");
 const roleMiddleware = require("../middleware/roleMiddleware");
-router.get("/",  authMiddleware, roleMiddleware('admin', 'user'), jadwalController.getAllJadwal);
+router.get("/",  authMiddleware, roleMiddleware('admin', ), jadwalController.getAllJadwal);
 router.post("/", authMiddleware, roleMiddleware('admin'), jadwalController.createJadwal);
-router.get("/:id", authMiddleware, roleMiddleware('admin','user'), jadwalController.getJadwalById);
+router.get("/:id", authMiddleware, roleMiddleware('admin'), jadwalController.getJadwalById);
 router.put("/:id", authMiddleware, roleMiddleware("admin"), jadwalController.updateJadwal);
 router.delete("/:id", authMiddleware, roleMiddleware("admin"),jadwalController.deleteJadwal);
 
