b93102e5313426fd7ca9a52f2654c6477ee054d7
fitriaulianisa <aulianisafitri434@gmail.com>
2025-01-03 18:06:40 +0700

    update

--- bimbel_api/routes/materi.js
+++ bimbel_api/routes/materi.js
@@ -9,16 +9,16 @@ const upload = require ("../middleware/uploadMiddleware");
 const authMiddleware = require("../middleware/authMiddleware");
 const roleMiddleware = require("../middleware/roleMiddleware");
 
-// router.get("/",  materiController.getAllMateri);
-// router.post("/", authMiddleware, roleMiddleware('admin'),  materiController.createMateri);
-// router.get("/:id", authMiddleware, materiController.getMateriById);
-// router.put("/:id", authMiddleware, roleMiddleware("admin"), materiController.updateMateri);
-// router.delete("/:id", authMiddleware, roleMiddleware("admin"),materiController.deleteMateri);
+router.get("/",  materiController.getAllMateri);
+router.post("/", authMiddleware, roleMiddleware('admin'),  materiController.createMateri);
+router.get("/:id", authMiddleware, materiController.getMateriById);
+router.put("/:id", authMiddleware, roleMiddleware("admin"), materiController.updateMateri);
+router.delete("/:id", authMiddleware, roleMiddleware("admin"),materiController.deleteMateri);
 
 
-router.get("/", materiController.getAllMateri);
-router.post("/",  materiController.createMateri);
-router.get("/:id", materiController.getMateriById);
-router.put("/:id", materiController.updateMateri);
-router.delete("/:id",  materiController.deleteMateri);
+// router.get("/", materiController.getAllMateri);
+// router.post("/",  materiController.createMateri);
+// router.get("/:id", materiController.getMateriById);
+// router.put("/:id", materiController.updateMateri);
+// router.delete("/:id",  materiController.deleteMateri);
 module.exports = router;
